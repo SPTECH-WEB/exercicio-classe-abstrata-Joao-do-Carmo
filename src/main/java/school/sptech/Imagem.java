8cfffc7a480f0c26e33feefddb5a4da03860376f
Joao-do-Carmo <joao.carmo@sptech.school>
2025-10-31 13:00:29 -0300

    Atividade finalizada - João Carmo - 04251066

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,45 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras = new ArrayList<>();
+
+    public Imagem() {
+    }
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double valor = 0.0;
+        for (Figura fig : figuras){
+            valor += fig.calcularArea();
+        }
+        return valor;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> figurasmaior = new ArrayList<>();
+        Double valor = 0.0;
+        for (Figura fig : figuras){
+            valor = fig.calcularArea();
+            if (valor > 20){
+                figurasmaior.add(fig);
+            }
+        }
+        return figurasmaior;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> quadrados = new ArrayList<>();
+        for (Figura fig : figuras){
+            if (fig instanceof Quadrado){
+                quadrados.add(fig);
+            }
+        }
+        return quadrados;
+    }
 }
--- src/main/java/school/sptech/Quadrado.java
+++ src/main/java/school/sptech/Quadrado.java
@@ -1,4 +1,28 @@
 package school.sptech;
 
-public class Quadrado {
+public class Quadrado extends Figura {
+    private Double lado;
+
+    public Quadrado() {
+    }
+
+    public Double getLado() {
+        return lado;
+    }
+
+    public void setLado(Double lado) {
+        this.lado = lado;
+    }
+
+    @Override public Double calcularArea(){
+        Double area = lado * lado;
+        return area;
+    }
+
+    @Override
+    public String toString() {
+        return "Quadrado{" +
+                "lado=" + lado +
+                '}';
+    }
 }
